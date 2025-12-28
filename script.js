2b0b99e19889c75bcb15b3cfe1cbf06245af7245
Amit Biswal <108086198+amitbiswal007@users.noreply.github.com>
2025-12-28 16:52:30 +0000

    added functionality of buttons

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+const copyBtn = document.getElementById('copyBtn')
+const newBtn = document.getElementById('newBtn')
+const reasonText = document.getElementById('reasonText')
+
 function getReasons() {
     const apiURL = 'https://naas.isalman.dev/no'
     const reasonDiv = document.getElementById('reason-text')
 
 
-    let apiResponse = fetch(apiURL)
+    let apiResponse = fetch(apiURL) //TODO - This gives CORS error, need to fix it at API layer
 
 
 }
 
 getReasons();
+
+newBtn.addEventListener('click', getReasons)
+
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(reasonText.innerText)
+})
+
