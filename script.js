1f1fc6f83c506fec92590f7ab57b81db72a5f028
Amit Biswal <108086198+amitbiswal007@users.noreply.github.com>
2025-12-28 08:29:16 +0000

    unfinished code

---
+++ script.js
@@ -0,0 +1,11 @@
+function getReasons() {
+    const apiURL = 'https://naas.isalman.dev/no'
+    const reasonDiv = document.getElementById('reason-text')
+
+
+    let apiResponse = fetch(apiURL)
+
+
+}
+
+getReasons();
